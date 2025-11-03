ee587938b56a5ca2a279da23c6e3f52ca8dcf8e8
nanzibabondstein-ops <nanziba.bondstein@gmail.com>
2025-11-03 16:58:35 +0600

    test

--- components/pages/home/AppSection.tsx
+++ components/pages/home/AppSection.tsx
@@ -1,53 +1,68 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function AppSection() {
-    return (
-        <div className='component-container mx-auto relative 2xl:pt-[76px] xl:pt-12 lg:pt-6 md:pb-0 pb-10 pt-10 2xl:mb-28'>
-            <div className='grid lg:grid-cols-2 grid-cols-1 bg-[#F0F4F9] 2xl:py-32 xl:py-12 lg:py-10 2xl:px-[7.14rem] xl:px-16 lg:px-11 p-0 rounded-4xl'>
-                <div className='space-y-5 px-4 pt-4 lg:px-0 lg:pt-0'>
-                    <h2 className='text-title'>Driven by Trust, Backed by Experience</h2>
-                    <p className='text-subtitle md:max-w-[33.4rem]'>I thought tracking would be complicated, but my 70-year-old father uses it better than me.</p>
-                    <div className='flex justify-start gap-6 mt-7'>
-                        <Image
-                            className='xl:w-auto lg:w-[100px] lg:h-[35px] xl:h-auto'
-                            height={58}
-                            width={195}
-                            src="/images/appleStore.png"
-                            alt="Apple Store"
-                        />
-                        <Image
-                            className='xl:w-auto lg:w-[100px] lg:h-[35px] xl:h-auto'
-                            height={58}
-                            width={195}
-                            src="/images/appStore.png"
-                            alt="Apple Store"
-                        />
-                    </div>
-                </div>
-                <div className='lg:block hidden'>
-                    <Image
-                        className='h-full max-w-min object-contain ml-auto 2xl:-translate-x-24 lg:-translate-x-11 2xl:translate-y-24 xl:translate-y-14 lg:translate-y-10'
-                        fill
-                        src="/images/mobile.png"
-                        alt="Apple Store"
-                    />
-                </div>
+  return (
+    <div className="component-container mx-auto relative 2xl:pt-[76px] xl:pt-12 lg:pt-6 md:pb-0 pb-10 pt-10 2xl:mb-28">
+      <div className="grid lg:grid-cols-2 grid-cols-1 bg-[#F0F4F9] 2xl:py-32 xl:py-12 lg:py-10 2xl:px-[7.14rem] xl:px-16 lg:px-11 p-0 rounded-4xl">
+        <div className="space-y-5 px-4 pt-4 lg:px-0 lg:pt-0">
+          <h2 className="text-title">Driven by Trust, Backed by Experience</h2>
+          <p className="text-subtitle md:max-w-[33.4rem]">
+            I thought tracking would be complicated, but my 70-year-old father
+            uses it better than me.
+          </p>
+          <div className="flex justify-start gap-6 mt-7">
+            <a
+              href="https://apps.apple.com/us/app/track-my-vehicle-tmv/id6670245303"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className="xl:w-auto lg:w-[100px] lg:h-[35px] xl:h-auto"
+                height={58}
+                width={195}
+                src="/images/appleStore.png"
+                alt="Apple Store"
+              />
+            </a>
 
-                <div className='lg:hidden block '>
-                    <Image
-                        className='mx-auto translate-y-10'
-                        height={353}
-                        width={278}
-                        src="/images/mobile.png"
-                        alt="Apple Store"
-                    />
-                </div>
-            </div>
+            <a
+              href="https://play.google.com/store/apps/details?id=com.bondstein.app.tmv"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className="xl:w-auto lg:w-[100px] lg:h-[35px] xl:h-auto"
+                height={58}
+                width={195}
+                src="/images/appStore.png"
+                alt="Google Play Store"
+              />
+            </a>
+          </div>
         </div>
-    )
-}
+        <div className="lg:block hidden">
+          <Image
+            className="h-full max-w-min object-contain ml-auto 2xl:-translate-x-24 lg:-translate-x-11 2xl:translate-y-24 xl:translate-y-14 lg:translate-y-10"
+            fill
+            src="/images/mobile.png"
+            alt="Apple Store"
+          />
+        </div>
 
+        <div className="lg:hidden block ">
+          <Image
+            className="mx-auto translate-y-10"
+            height={353}
+            width={278}
+            src="/images/mobile.png"
+            alt="Apple Store"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 /* 
 
--- components/pages/product-details/ProductDescription.tsx
+++ components/pages/product-details/ProductDescription.tsx
@@ -1,92 +1,128 @@
-import { AspectRatio } from '@/components/ui/aspect-ratio';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import Image from 'next/image';
-import React from 'react'
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Image from "next/image";
+import React from "react";
 
 interface ProductSpecifications {
-    productImage: string;
-    description: string[];
-    gallery: string[];
-    specifications: string[];
-    productName: string;
+  productImage: string;
+  description: string[];
+  gallery: string[];
+  specifications: string[];
+  productName: string;
 }
-export default function ProductDescription({ productSpecifications }: { productSpecifications: ProductSpecifications }) {
-    const { productImage = "/images/storePlaceholder.png", productName, description, gallery, specifications } = productSpecifications;
-    return (
-        <div className="component-container mx-auto lg:my-24 my-7">
-            <Tabs defaultValue="description" className="bg-[#F0F2F5] lg:p-12 sm:p-4 p-2 rounded-lg w-full">
-                <TabsList className="mx-0 w-full bg-transparent lg:h-[60px] h-[38px]">
-                    <TabsTrigger value="description" className="py-4 custom-tab lg:h-[60px] h-[38px] lg:text-2xl sm:text-base xs:text-base text-[12px] font-semibold">
-                        Description
-                    </TabsTrigger>
-                    <TabsTrigger value="gallery" className="py-4 custom-tab lg:h-[60px] h-[38px] lg:text-2xl sm:text-base xs:text-base text-[10px] font-semibold" >Product Gallery</TabsTrigger>
-                    <TabsTrigger value="specifications" className="py-4 custom-tab lg:h-[60px] h-[38px] lg:text-2xl sm:text-base xs:text-base text-[10px] font-semibold">Specifications</TabsTrigger>
-                </TabsList>
-                <TabsContent value="description" className=''>
-                    <div className='lg:grid flex flex-col-reverse grid-cols-2 bg-[#F0F2F5]'>
-                        <div className="bg-[#F0F2F5] p-4">
-                            <p className='text-lg lg:text-xl font-semibold md:text-2xl mb-4'>{productName} Features:</p>
-                            <ul className='list-disc space-y-4 px-4 py-2'>
-                                {description.map((desc, index) => (
-                                    <li key={index} className='text-base leading-7 text-[#687177] font-normal'>
-                                        {desc}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className='bg-[#F0F2F5]'>
-                            <AspectRatio ratio={1} className="w-full">
-                                <Image
-                                    src={productImage}
-                                    alt="Product Image"
-                                    fill
-                                    className="object-contain w-full h-full"
-                                />
-                            </AspectRatio>
-                        </div>
-                    </div>
-                </TabsContent>
-                <TabsContent value="gallery">
-                    <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 bg-[#F0F2F5] lg:max-h-auto gap-4 max-h-[80vh] overflow-x-auto '>
-                        {gallery.map((img = "/images/storePlaceholder.png", index) => (
-                            <div key={index}>
-                                <AspectRatio ratio={1} className="w-full">
-                                    <Image
-                                        src={img}
-                                        alt="Product Image"
-                                        fill
-                                        className="object-contain w-full h-full"
-                                    />
-                                </AspectRatio>
-                            </div>
-                        ))}
-                    </div>
-                </TabsContent>
-                <TabsContent value="specifications">
-                    <div className='lg:grid flex flex-col-reverse grid-cols-2 bg-[#F0F2F5]'>
-                        <div className="bg-[#F0F2F5] p-4">
-                            <p className='text-lg lg:text-xl font-semibold md:text-2xl mb-4'>{productName} specifications:</p>
-                            <ul className='list-disc space-y-4 px-4 py-2'>
-                                {specifications.map((desc, index) => (
-                                    <li key={index} className='text-base lg: leading-7 text-[#687177] font-normal'>
-                                        {desc}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className='bg-[#F0F2F5]'>
-                            <AspectRatio ratio={1} className="w-full">
-                                <Image
-                                    src={productImage}
-                                    alt="Product Image"
-                                    fill
-                                    className="object-contain w-full h-full"
-                                />
-                            </AspectRatio>
-                        </div>
-                    </div>
-                </TabsContent>
-            </Tabs>
-        </div>
-    )
+export default function ProductDescription({
+  productSpecifications,
+}: {
+  productSpecifications: ProductSpecifications;
+}) {
+  const {
+    productImage = "/images/storePlaceholder.png",
+    productName,
+    description,
+    gallery,
+    specifications,
+  } = productSpecifications;
+  return (
+    <div className="component-container mx-auto lg:my-24 my-7">
+      <Tabs
+        defaultValue="description"
+        className="bg-[#F0F2F5] lg:p-12 sm:p-4 p-2 rounded-lg w-full"
+      >
+        <TabsList className="mx-0 w-full bg-transparent lg:h-[60px] h-[38px]">
+          <TabsTrigger
+            value="description"
+            className="py-4 custom-tab lg:h-[60px] h-[38px] lg:text-2xl sm:text-base xs:text-base text-[12px] font-semibold cursor-pointer"
+          >
+            Description
+          </TabsTrigger>
+          <TabsTrigger
+            value="gallery"
+            className="py-4 custom-tab lg:h-[60px] h-[38px] lg:text-2xl sm:text-base xs:text-base text-[10px] font-semibold cursor-pointer "
+          >
+            Product Gallery
+          </TabsTrigger>
+          <TabsTrigger
+            value="specifications"
+            className="py-4 custom-tab lg:h-[60px] h-[38px] lg:text-2xl sm:text-base xs:text-base text-[10px] font-semibold cursor-pointer"
+          >
+            Specifications
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="description" className="">
+          <div className="lg:grid flex flex-col-reverse grid-cols-2 bg-[#F0F2F5]">
+            <div className="bg-[#F0F2F5] p-4">
+              <p className="text-lg lg:text-xl font-semibold md:text-2xl mb-4">
+                {productName} Features:
+              </p>
+              <ul className="list-disc space-y-4 px-4 py-2">
+                {description.map((desc, index) => (
+                  <li
+                    key={index}
+                    className="text-base leading-7 text-[#687177] font-normal"
+                  >
+                    {desc}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="bg-[#F0F2F5]">
+              <AspectRatio ratio={1} className="w-full">
+                <Image
+                  src={productImage}
+                  alt="Product Image"
+                  fill
+                  className="object-contain w-full h-full"
+                />
+              </AspectRatio>
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="gallery">
+          <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 bg-[#F0F2F5] lg:max-h-auto gap-4 max-h-[80vh] overflow-x-auto ">
+            {gallery.map((img = "/images/storePlaceholder.png", index) => (
+              <div key={index}>
+                <AspectRatio ratio={1} className="w-full">
+                  <Image
+                    src={img}
+                    alt="Product Image"
+                    fill
+                    className="object-contain w-full h-full"
+                  />
+                </AspectRatio>
+              </div>
+            ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="specifications">
+          <div className="lg:grid flex flex-col-reverse grid-cols-2 bg-[#F0F2F5]">
+            <div className="bg-[#F0F2F5] p-4">
+              <p className="text-lg lg:text-xl font-semibold md:text-2xl mb-4">
+                {productName} specifications:
+              </p>
+              <ul className="list-disc space-y-4 px-4 py-2">
+                {specifications.map((desc, index) => (
+                  <li
+                    key={index}
+                    className="text-base lg: leading-7 text-[#687177] font-normal"
+                  >
+                    {desc}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="bg-[#F0F2F5]">
+              <AspectRatio ratio={1} className="w-full">
+                <Image
+                  src={productImage}
+                  alt="Product Image"
+                  fill
+                  className="object-contain w-full h-full"
+                />
+              </AspectRatio>
+            </div>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
