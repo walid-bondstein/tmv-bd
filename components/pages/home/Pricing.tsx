1f88a858fd77d3ce312582aa9537b6d417833c4f
Sami An <samian@Samis-MacBook-Pro.local>
2026-02-09 14:28:21 +0600

    fix(main): greed issue

--- components/pages/home/Pricing.tsx
+++ components/pages/home/Pricing.tsx
@@ -10,15 +10,15 @@ export default function Pricing({ products }: {
                 <h2 className="text-title text-center">Find the right solution</h2>
                 <p className="text-subtitle text-center">Explore products designed to power your fleet’s success.</p>
             </div>
-            <section className="md:grid hidden grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 my-10">
+            <section className="lg:grid hidden grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 my-10">
                 {
                     products.map((product) => (
                         <PricingCard key={product.id} product={product} />
                     ))
                 }
             </section>
-            <section className="md:hidden block overflow-x-auto">
-                <section className="flex justify-center border border-red-500 min-w-max items-center gap-4 my-3">
+            <section className="lg:hidden block overflow-x-auto">
+                <section className="flex justify-center min-w-max items-center gap-4 my-3">
                     {
                         products.map((product) => (
                             <PricingCard key={product.id} product={product} />
